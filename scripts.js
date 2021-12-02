7dd9caaf91ba9b32ae4d3a01e776a24710d001cf
raghavraj-27 <raghvendraprasad5678@gmail.com>
2021-12-02 19:33:53 +0530

    file changed

--- scripts.js
+++ scripts.js
@@ -1,34 +1,21 @@
-const img_1 = "/images/dice1";
-const img_2 = "/images/dice1";
-const img_3 = "/images/dice1";
-const img_4 = "/images/dice1";
-const img_5 = "/images/dice1";
-const img_6 = "/images/dice1";
-
-var load_1, load_2;
-
-function check(val) {
-    if(val === 1) {
-        return img_1;
-    } else if(val == 2) {
-        return img_2;
-    } else if(val === 3) {
-        return img_3;
-    } else if(val === 4) {
-        return img_4;
-    } else if(val === 5) {
-        return img_5;
-    } 
-    return img_6;
-}
-
 function refresh() {
-    var p1 = Math.random(); 
-    var p1 = floor(p1) * 6 +1;
+    var p1 = Math.random() * 6; 
+    p1 = Math.floor(p1) + 1;
 
-    var p2 = Math.random(); 
-    var p2 = floor(p2) * 6 +1;
+    var p2 = Math.random() * 6; 
+    p2 = Math.floor(p2) + 1;
 
-    load_1 = check(p1);
-    load_2 = check(p2);
-}
+    document.querySelector('.img1').setAttribute("src", "./images/dice" + p1 + ".png");
+    document.querySelector('.img2').setAttribute("src", "./images/dice" + p2 + ".png");
+
+    if(p1 > p2) {
+      document.getElementById("p1").innerHTML += " Wins !";
+      document.getElementById("p2").innerHTML += " Lose !";
+    } else if(p1 < p2) {
+      document.getElementById("p1").innerHTML += " Lose !";
+      document.getElementById("p2").innerHTML += " Wins !";
+    } else {
+      document.getElementById("p1").innerHTML += " Wins !";
+      document.getElementById("p2").innerHTML += " Wins !";
+    }
+  }
